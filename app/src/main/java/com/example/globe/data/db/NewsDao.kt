21791798d87a0d34cbab5441570cc9950b727a0e
pulkitrathi17 <pulkitrathi98@gmail.com>
2019-02-17 19:22:08 +0530

    minor

--- app/src/main/java/com/example/globe/data/db/NewsDao.kt
+++ app/src/main/java/com/example/globe/data/db/NewsDao.kt
@@ -14,19 +14,7 @@ interface NewsDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun upsertArticles(articles: List<Article>)
 
-/*    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insertSources(sources: List<SourceModel>)*/
-
-/*    @Query("SELECT COUNT(*) FROM sourcemodel")
-    fun getSourcesCount():Int*/
-
     @Query("SELECT * FROM article ORDER BY publishedAt DESC")
     fun getArticles(): LiveData<Article>
- //   fun getAllArticles(): DataSource.Factory<Int, Article>
 
-/*    @Query("SELECT * FROM sourcemodel ORDER BY name ASC")
-    fun getAllSources(): LiveData<List<SourceModel>>*/
-
-    @Query("SELECT * FROM article ORDER BY publishedAt DESC LIMIT 1")
-    fun getTopArticle(): Article
 }
